3dfed876e79af07b4075c7f7f61b8bbc73cc4cea
Xavier Ordoquy <xordoquy@linovia.com>
2013-02-08 11:04:23 +0100

    urlparse compat for python3.

--- raven/utils/urlparse.py
+++ raven/utils/urlparse.py
@@ -1,6 +1,10 @@
 from __future__ import absolute_import
-import urlparse as _urlparse
 
+try:
+    import urlparse as _urlparse
+except ImportError:
+    from urllib import parse as _urlparse
+ 
 
 def register_scheme(scheme):
     for method in filter(lambda s: s.startswith('uses_'), dir(_urlparse)):
